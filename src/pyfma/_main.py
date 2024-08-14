e12d69d97a97657ab4fec3e8f2b2859f4360bc03
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2024-08-14 12:46:36 -0500

    Replace deprecated find_common_type

--- src/pyfma/_main.py
+++ src/pyfma/_main.py
@@ -2,12 +2,13 @@ import _pyfma
 import numpy as np
 from numpy.typing import ArrayLike
 
+p = np.promote_types
 
 def fma(a: ArrayLike, b: ArrayLike, c: ArrayLike) -> np.ndarray:
     a = np.asarray(a)
     b = np.asarray(b)
     c = np.asarray(c)
-    dtype = np.find_common_type([], [a.dtype, b.dtype, c.dtype])
+    dtype = p(p(a.dtype, b.dtype), c.dtype)
     a = a.astype(dtype)
     b = b.astype(dtype)
     c = c.astype(dtype)
